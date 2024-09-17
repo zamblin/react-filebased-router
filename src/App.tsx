d27499da1d01e3ebdd2c22fd7fec2fb032593a78
dutexion <dutexion@gmail.com>
2024-09-18 03:27:10 +0900

    success

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,53 @@
-export const App = () => {
-  return <div>Hello world</div>;
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  RouteObject,
+} from 'react-router-dom';
+
+interface PageModule {
+  default: React.ComponentType;
+  loader?: () => Promise<any>;
+  action?: () => Promise<any>;
+  ErrorBoundary?: React.ComponentType;
+}
+
+declare const import_meta: {
+  glob: (
+    path: string,
+    options: { eager: boolean }
+  ) => Record<string, PageModule>;
 };
+
+const pages = import.meta.glob<PageModule>('./pages/**/*.tsx', { eager: true });
+
+const routes: RouteObject[] = [];
+
+for (const path of Object.keys(pages)) {
+  const fileName = path.match(/\.\/pages\/(.*)\.tsx$/)?.[1];
+  if (!fileName) {
+    continue;
+  }
+
+  const normalizedPathName = fileName.includes('$')
+    ? fileName.replace('$', ':')
+    : fileName.replace(/\/index/, '');
+
+  routes.push({
+    path: fileName === 'index' ? '/' : `/${normalizedPathName.toLowerCase()}`,
+    element: React.createElement(pages[path].default),
+    loader: pages[path]?.loader,
+    action: pages[path]?.action,
+    errorElement: pages[path]?.ErrorBoundary
+      ? React.createElement(pages[path].ErrorBoundary!)
+      : undefined,
+  });
+}
+
+const router = createBrowserRouter(routes);
+
+const App: React.FC = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
